ff0efb95f792dc53de0e8f4953ebc31e9756798d
Datumza <reitumetsemasilo@gmail.com>
2025-01-06 13:09:11 +0200

    fix: Rename className and add icon

--- contigion_charts/components/text.py
+++ contigion_charts/components/text.py
@@ -1,26 +1,31 @@
-from dash.html import H1, H2, H3, P, A
+from dash.html import H1, H2, H3, P, A, I
 
 
-def title(title_id, text_input, className=''):
-    classes = ' '.join(['component text', className])
+def title(title_id, text_input, class_name=''):
+    classes = ' '.join(['component text', class_name])
     return H1(text_input, id=title_id, className=classes)
 
 
-def heading(heading_id, text_input, className=''):
-    classes = ' '.join(['component text', className])
+def heading(heading_id, text_input, class_name=''):
+    classes = ' '.join(['component text', class_name])
     return H2(text_input, id=heading_id, className=classes)
 
 
-def sub_heading(sub_heading_id, text_input, className=''):
-    classes = ' '.join(['component text', className])
+def sub_heading(sub_heading_id, text_input, class_name=''):
+    classes = ' '.join(['component text', class_name])
     return H3(text_input, id=sub_heading_id, className=classes)
 
 
-def text(text_id, text_input, className=''):
-    classes = ' '.join(['component text', className])
+def text(text_id, text_input, class_name=''):
+    classes = ' '.join(['component text', class_name])
     return P(text_input, id=text_id, className=classes)
 
 
-def link(link_id, text_input, href, className=''):
-    classes = ' '.join(['component text', className])
+def link(link_id, text_input, href, class_name=''):
+    classes = ' '.join(['component text', class_name])
     return A(text_input, href=href, id=link_id, className=classes)
+
+
+def icon(icon_id, class_name):
+    I(className=class_name, id=icon_id, n_clicks=0)
+
