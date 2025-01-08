d19866fcefbd0ce2874cf3b9f70dc055d64632f7
Datumza <reitumetsemasilo@gmail.com>
2025-01-08 20:53:06 +0200

    fix: indicator library alignment

--- contigion_charts/util/indicators.py
+++ contigion_charts/util/indicators.py
@@ -1,27 +1,26 @@
-from contigion_indicators import (macd_crossover, psar_trend, rsi, rsi_mavg, rsi_over_bought_sold, supertrend,
-                                  bollinger_bands, supertrend_direction, sma_crossover, sma_trend_direction,
-                                  triple_candlestick_pattern, get_support_and_resistance_levels,
+from contigion_indicators import (macd_crossover, psar_trend, rsi_signal, rsi_mavg, rsi_over_bought_sold, supertrend,
+                                  bollinger_bands_cross, supertrend_trend, sma_crossover, sma_trend,
+                                  triple_candlestick_pattern, support_and_resistance,
                                   candlestick_type, single_candlestick_pattern, dual_candlestick_pattern)
-from contigion_indicators import trading_session, day_of_the_week
 
 indicator_map = {
-    'Bollinger Bands': bollinger_bands,
+    'Bollinger Bands': bollinger_bands_cross,
     'SMA Crossover': sma_crossover,
-    'SMA Direction': sma_trend_direction,
+    'SMA Direction': sma_trend,
     'Supertrend': supertrend,
 
-    'RSI': rsi,
+    'RSI': rsi_signal,
     'RSI Moving Average': rsi_mavg,
     'RSI Overbought Oversold': rsi_over_bought_sold,
     'MACD Crossover': macd_crossover,
 
-    'Supertrend Direction': supertrend_direction,
+    'Supertrend Direction': supertrend_trend,
     'PSAR': psar_trend,
     'Candle Type': candlestick_type,
     'Candle Patterns (1x)': single_candlestick_pattern,
     'Candle Patterns (2x)': dual_candlestick_pattern,
     'Candle Patterns (3x)': triple_candlestick_pattern,
-    'Support and Resistence': get_support_and_resistance_levels,
+    'Support and Resistence': support_and_resistance,
 }
 
 
